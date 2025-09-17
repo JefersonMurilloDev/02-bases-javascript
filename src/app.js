2df958557bd30a4d57b09b10ea6c000974eddf98
Jefferson Murillo <jefferdaniel2001@gmail.com>
2025-09-16 20:24:01 -0500

    feat: Implementa logger con Winston para el registro de eventos

--- src/app.js
+++ src/app.js
@@ -9,20 +9,16 @@
 //     console.log({ user });
 // });
 // const { buildMakePerson } = require('./js-foundation/05-factory');
-const getPokemonById = require('./js-foundation/06-promises');
+// const getPokemonById = require('./js-foundation/06-promises');
+// getPokemonById(1000)
+//     .then( ( pokemon ) => console.log({ pokemon  }) )
+//     .catch( ( err ) => console.log(err) )
+//     .finally( () => console.log('Fin de getPokemonById') );
+const { buildLogger } = require('./plugins');
+
+const logger = buildLogger('app.js');
+
+logger.log('Hola Mundo');
+logger.error('Esto es algo malo');
 
 
-getPokemonById(1000)
-    .then( ( pokemon ) => console.log({ pokemon  }) )
-    .catch( ( err ) => console.log(err) )
-    .finally( () => console.log('Fin de getPokemonById') );
-
-//! Referencias a la funtion factory and case use;
-// instancia concreta con los plugins que ya tenemos
-// const makePerson = buildMakePerson({ getUUID, getAge });
-
-// const obj = { name: 'John', birthdate: '2000-02-21' };
-
-// const john = makePerson(obj);
-
-// console.log({ john });  
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,10 +1,12 @@
 const { getAge } = require('../plugins/get-age.plugin');
 const { getUUID } = require('../plugins/get-uuid.plugin');
 const { http } = require('../plugins/http-client.plugin');
+const buildLogger = require('../plugins/logger.plugin');
 
 
 module.exports = {
     getAge,
     getUUID,
-    http  
+    http,
+    buildLogger
 };
